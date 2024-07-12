15698269a6b5c8c1d27a3b9bb594e0f5782cf741
Alexander Konovalov <alexxonov@gmail.com>
2024-07-12 22:35:29 +0500

    Optimize images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header: React.FC = () => {
-  const [expanded, setExpanded] = React.useState(false);
-  const [toggled, setToggled] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const [toggled, setToggled] = useState(false);
 
   const onClick = () => {
     if (!toggled) {
